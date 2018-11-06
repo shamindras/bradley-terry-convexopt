e0ce36025df01a978c9fb7ff78abef3413a48883
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2018-11-06 00:28:36 -0500

    ENH: Setup code to run for all seasons and 16 rounds

--- R/get_nfl_data.R
+++ R/get_nfl_data.R
@@ -1,3 +1,7 @@
+# Clean up
+rm(list = ls())
+cat("\014")
+
 # The following script will give us the pairwise Bradley-Terry
 # model data for NFL
 
@@ -8,7 +12,9 @@
 
 # Access tidyverse:
 # install.packages("tidyverse")
+# install.packages("here")
 library(tidyverse)
+library(here)
 
 # Access nflWAR:
 # install.packages("devtools")
@@ -54,134 +60,194 @@ games_data %>%
 # We can also do this to get a crude average games per season
 dim(games_data)[1]/length(all_seasons)
 
-#-------------------------------------------------------------------
-# Basic checks on Data
-#-------------------------------------------------------------------
+get_single_season_data <- function(games_data, rseason){
 
-# Consider a specific (required) season
-rseason <- 2016
+    #-------------------------------------------------------------------
+    # Basic checks on Data
+    #-------------------------------------------------------------------
 
-# Get only the games for the required season
-games_data_rseason <- games_data %>%
-                        dplyr::filter(Season == rseason)
+    # Get only the games for the required season
+    games_data_rseason <- games_data %>%
+        dplyr::filter(Season == rseason)
 
 
-#-------------------------------------------------------------------
-# CREATE CORE DATA for a SEASON
-#-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    # CREATE CORE DATA for a SEASON
+    #-------------------------------------------------------------------
 
-# We want to get an ordering of teams and a uniq team number for
-# the season
-unq_teams <- games_data_rseason %>%
-                dplyr::select(home) %>%
-                dplyr::distinct(.) %>%
-                dplyr::rename(team = home) %>%
-                dplyr::arrange(team) %>%
-                dplyr::mutate(ind = 1:n())
+    # We want to get an ordering of teams and a uniq team number for
+    # the season
+    unq_teams <- games_data_rseason %>%
+        dplyr::select(home) %>%
+        dplyr::distinct(.) %>%
+        dplyr::rename(team = home) %>%
+        dplyr::arrange(team) %>%
+        dplyr::mutate(ind = 1:n())
 
-View(unq_teams)
+    # Get Home teams as the primary "team" column
+    games_home <- games_data_rseason %>%
+        dplyr::rename(team = home,
+                      team_other = away,
+                      team_score = homescore,
+                      team_other_score = awayscore) %>%
+        dplyr::select(GameID, date, team, team_other,
+                      team_score, team_other_score,
+                      Season)
 
-# Get Home teams as the primary "team" column
-games_home <- games_data_rseason %>%
-                dplyr::rename(team = home,
-                              team_other = away,
-                              team_score = homescore,
-                              team_other_score = awayscore) %>%
-                dplyr::select(GameID, date, team, team_other,
-                              team_score, team_other_score,
-                              Season)
+    # Get Away teams as the primary "team" column
+    games_away <- games_data_rseason %>%
+        dplyr::rename(team = away,
+                      team_other = home,
+                      team_score = awayscore,
+                      team_other_score = homescore) %>%
+        dplyr::select(GameID, date, team, team_other,
+                      team_score, team_other_score,
+                      Season)
 
-# Get Away teams as the primary "team" column
-games_away <- games_data_rseason %>%
-                dplyr::rename(team = away,
-                              team_other = home,
-                              team_score = awayscore,
-                              team_other_score = homescore) %>%
-                dplyr::select(GameID, date, team, team_other,
-                              team_score, team_other_score,
-                              Season)
+    # Combine into a single dataframe and create the "round" for each team
+    # based on play date
+    games_all <- games_home %>%
+        dplyr::bind_rows(games_away) %>%
+        dplyr::group_by(team) %>%
+        dplyr::mutate(round = row_number(date))
 
-# Combine into a single dataframe and create the "round" for each team
-# based on play date
-games_all <- games_home %>%
-                dplyr::bind_rows(games_away) %>%
-                dplyr::group_by(team) %>%
-                dplyr::mutate(round = row_number(date))
+    # Join on the unique team id number created for the "primary" team
+    games_all <- games_all %>%
+        dplyr::left_join(x = ., y = unq_teams, by = c("team")) %>%
+        dplyr::rename(team_ind = ind)
 
-# Join on the unique team id number created for the "primary" team
-games_all <- games_all %>%
-                dplyr::left_join(x = ., y = unq_teams, by = c("team")) %>%
-                dplyr::rename(team_ind = ind)
+    # Join on the unique team id number created for the "other" team
+    # diff indicator - currently gives 0 value for a tie
+    # TODO: Check how we will deal with ties in our model
 
-# Join on the unique team id number created for the "other" team
-# diff indicator - currently gives 0 value for a tie
-# TODO: Check how we will deal with ties in our model
+    games_all <- games_all %>%
+        dplyr::left_join(x = ., y = unq_teams, by = c("team_other" = "team")) %>%
+        dplyr::rename(team_other_ind = ind) %>%
+        dplyr::mutate(diff = team_score - team_other_score,
+                      diff_sign = sign(diff))
 
-games_all <- games_all %>%
-                dplyr::left_join(x = ., y = unq_teams, by = c("team_other" = "team")) %>%
-                dplyr::rename(team_other_ind = ind) %>%
-                dplyr::mutate(diff = team_score - team_other_score,
-                              diff_sign = sign(diff))
+    #-------------------------------------------------------------------
+    # CREATE CARTESIAN pairwise master dataframe
+    #-------------------------------------------------------------------
 
-View(games_all)
+    all_rounds <- games_all %>%
+        dplyr::ungroup() %>%
+        dplyr::select(round) %>%
+        dplyr::distinct() %>%
+        dplyr::arrange(round)
 
-#-------------------------------------------------------------------
-# CREATE CARTESIAN pairwise master dataframe
-#-------------------------------------------------------------------
+    all_team_ind <- unq_teams %>%
+        dplyr::ungroup() %>%
+        dplyr::select(ind) %>%
+        dplyr::distinct() %>%
+        dplyr::arrange(ind) %>%
+        dplyr::rename(team_ind = ind)
 
-all_rounds <- games_all %>%
-                dplyr::ungroup() %>%
-                dplyr::select(round) %>%
-                dplyr::distinct() %>%
-                dplyr::arrange(round)
+    all_team_other_ind <- unq_teams %>%
+        dplyr::ungroup() %>%
+        dplyr::select(ind) %>%
+        dplyr::distinct() %>%
+        dplyr::arrange(ind) %>%
+        dplyr::rename(team_other_ind = ind)
 
-all_team_ind <- unq_teams %>%
-                    dplyr::ungroup() %>%
-                    dplyr::select(ind) %>%
-                    dplyr::distinct() %>%
-                    dplyr::arrange(ind) %>%
-                    dplyr::rename(team_ind = ind)
+    # cartesian product
+    all_round_team_combs <- all_rounds %>%
+        tidyr::crossing(all_team_ind) %>%
+        tidyr::crossing(all_team_other_ind)
 
-all_team_other_ind <- unq_teams %>%
-                        dplyr::ungroup() %>%
-                        dplyr::select(ind) %>%
-                        dplyr::distinct() %>%
-                        dplyr::arrange(ind) %>%
-                        dplyr::rename(team_other_ind = ind)
+    out_list <- list("games_all" = games_all,
+                     "all_round_team_combs" = all_round_team_combs)
 
-# cartesian product
-all_round_team_combs <- all_rounds %>%
-                            tidyr::crossing(all_team_ind) %>%
-                            tidyr::crossing(all_team_other_ind)
+    base::return(out_list)
+}
 
-# Unit test: cartesian product - should be 0
-# (just product of row dimensions less total dimension)
-dim(all_rounds)[1] * dim(all_team_ind)[1] * dim(all_team_other_ind)[1] - dim(all_round_team_combs)[1]
+get_pairwise_diff_single_round <- function(single_season_data,
+                                           round_num){
 
-#-------------------------------------------------------------------
-# Create pairwise difference dataframes
-#-------------------------------------------------------------------
+    # Get the diffs for a single round for all teams
+    games_round <- single_season_data$games_all %>%
+        dplyr::ungroup() %>%
+        dplyr::filter(round == round_num) %>%
+        dplyr::select(team_ind, team_other_ind, diff, diff_sign) %>%
+        dplyr::arrange(team_ind)
 
-# Now do it for a single round
+    # Get a dataframe of all possible pairwise differences across teams
+    games_round_all_combs <- single_season_data$all_round_team_combs %>%
+        dplyr::ungroup() %>%
+        dplyr::filter(round == round_num) %>%
+        dplyr::select(team_ind, team_other_ind) %>%
+        dplyr::left_join(x = .,
+                         y = games_round,
+                         by = c("team_ind" = "team_ind",
+                                "team_other_ind" = "team_other_ind"))
+
+    out_df <- games_round_all_combs %>%
+        dplyr::select(-diff_sign)
+
+    base::return(out_df)
+}
+
+# Write files to output directory
 round_num <- 1
+rseason <- 2009
+out_nfl_dir <- here::here("data", "nfl", rseason)
+out_file_name <- stringr::str_c("round",
+                                stringr::str_pad(round_num,
+                                                 width = 2,
+                                                 side = "left",
+                                                 pad = "0"),
+                                sep = "_") %>%
+                    stringr::str_c(., ".csv")
+out_file_path <- base::file.path(out_nfl_dir, out_file_name)
+readr::write_csv(x = dat_rseason[[1]], path = out_file_path)
 
-# Get the diffs for a single round for all teams
-games_round <- games_all %>%
-                    dplyr::ungroup() %>%
-                    dplyr::filter(round == round_num) %>%
-                    dplyr::select(team_ind, team_other_ind, diff, diff_sign) %>%
-                    dplyr::arrange(team_ind)
+write_csv_diffs <- function(rseason, round_num, all_rounds_rseason){
+    out_nfl_dir <- here::here("data", "nfl", rseason)
+    out_file_name <- stringr::str_c("round",
+                                    stringr::str_pad(round_num,
+                                                     width = 2,
+                                                     side = "left",
+                                                     pad = "0"),
+                                    sep = "_") %>%
+        stringr::str_c(., ".csv")
+    out_file_path <- base::file.path(out_nfl_dir, out_file_name)
+    readr::write_csv(x = all_rounds_rseason[[round_num]],
+                     path = out_file_path)
+}
 
-# Get a dataframe of all possible pairwise differences across teams
-games_round_all_combs <- all_round_team_combs %>%
-                            dplyr::ungroup() %>%
-                            dplyr::filter(round == round_num) %>%
-                            dplyr::select(team_ind, team_other_ind) %>%
-                            dplyr::left_join(x = .,
-                                             y = games_round,
-                                             by = c("team_ind" = "team_ind",
-                                                    "team_other_ind" = "team_other_ind"))
-View(games_round_all_combs)
+# Generate output for a single season
+rseason <- 2009
+round_nums <- 1:16
 
-games_round_all_combs %>%
-    dplyr::select(-diff_sign)
+dat_rseason <- get_single_season_data(games_data = games_data,
+                                   rseason = rseason)
+
+all_rounds_rseason <- purrr::map(round_nums,
+                                 ~get_pairwise_diff_single_round(single_season_data = dat_rseason,
+                                                                 round_num = .x))
+
+# Write out all rounds for the season
+purrr::walk(.x = round_nums,
+            ~write_csv_diffs(rseason = rseason,
+                             round_num = .x,
+                             all_rounds_rseason=all_rounds_rseason))
+
+create_single_season_csv <- function(rseason, round_nums){
+    dat_rseason <- get_single_season_data(games_data = games_data,
+                                          rseason = rseason)
+
+    all_rounds_rseason <- purrr::map(round_nums,
+                                     ~get_pairwise_diff_single_round(single_season_data = dat_rseason,
+                                                                     round_num = .x))
+
+    # Write out all rounds for the season
+    purrr::walk(.x = round_nums,
+                ~write_csv_diffs(rseason = rseason,
+                                 round_num = .x,
+                                 all_rounds_rseason=all_rounds_rseason))
+}
+
+rseasons <- 2009:2010
+round_nums <- 1:16
+purrr::walk(.x = rseasons, ~create_single_season_csv(rseason = .x,
+                                                     round_nums = round_nums))
