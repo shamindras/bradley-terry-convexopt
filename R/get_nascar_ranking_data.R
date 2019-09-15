32edcd93f441a6ad85097a8ca6be06732a7854ad
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2019-09-14 22:51:50 -0400

    DOC: #36 Add nascar raw data to gitignore and also update R scripts for nascar 2002 data

--- R/get_nascar_ranking_data.R
+++ R/get_nascar_ranking_data.R
@@ -1,5 +1,12 @@
-# library(glue)
-# library(here)
+#!/usr/bin/env Rscript
+
+# Here we download the main NASCAR 2002 ranking dataset for all 36 races as
+# found here: https://fantasyracingcheatsheet.com/nascar/drivers/point-standings/2002
+# We do minor modifications to the column names and add columns for year (2002)
+# and the specific race number
+
+library(glue)
+library(here)
 library(tidyverse)
 
 #' Get NASCAR race ranking based on specific year and race number. All of the
@@ -39,13 +46,20 @@ NASCAR_YR <- 2002
 RACE_NUMS <- 1:36
 
 # Obtain a combined data frame of nascar rankings for all races in a single tibble
+print("Downloading the NASCAR 2002 rankings dataset...")
 nascar_2002_all_std <-
     RACE_NUMS %>%
     purrr::map_dfr(.x = .,
                    .f = ~get_nascar_standings(nsc_year = NASCAR_YR,
                                               nsc_race = .x)) %>%
     janitor::clean_names()
+print("Downloading DONE!")
 
+# Download the (raw) NASCAR 2002 rankings data
 # Write output to CSV format
+print("Writing the NASCAR 2002 rankings dataset...")
+
 readr::write_csv(x = nascar_2002_all_std,
                  path = here::here("data", "nascar", "nascar_2002_all_std.csv"))
+
+print(glue::glue("DONE!\nPlease see {here::here('data', 'nascar', 'nascar_2002_all_std.csv')} for the NASCAR 2002 ranking csv file"))
